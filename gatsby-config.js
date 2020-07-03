0304bc8f3a13dfeda4ba7fd41e8911d0d284bffb
Michael Zuercher <michzuerch@gmail.com>
2020-07-03 12:14:36 +0200

    env and md-files for blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,5 +10,15 @@ module.exports = {
     title: 'Portfolio',
     author: 'michzuerch'
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`,
+        ignore: [`**/\.*`]
+      }
+    }
+  ],
 }
