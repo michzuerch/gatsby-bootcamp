5faed4ea4c7e7051014e452adf82e036892c1063
Michael Zuercher <michzuerch@gmail.com>
2020-07-03 10:28:37 +0200

    query title works

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,24 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 //import "./header.scss" 
 import headerStyles from './header.module.scss'
 
 const Header = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
   return (
     <header className={headerStyles.header}>
       <h1> 
         <Link className={headerStyles.title} 
-        to="/">Michael Zuercher</Link>
+        to="/">{data.site.siteMetadata.title}</Link>
       </h1>
       <nav>
         <ul className={headerStyles.navList}>
