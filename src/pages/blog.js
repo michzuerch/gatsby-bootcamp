cd1faac9dccb151dde371de90103452796c61b57
Michael Zuercher <michzuerch@gmail.com>
2020-07-05 13:40:09 +0200

    blog titles and date von blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -19,11 +19,20 @@ const BlogPage = () => {
       }
     }
   `);
-  console.log(data);
+  // console.log(data);
 
   return (
     <Layout>
       <h1>Blog</h1>
+      <ol>
+        {data.allMarkdownRemark.edges.map((edge) => (
+          <li>
+            <h2>{edge.node.frontmatter.title}</h2>
+            <p>{edge.node.frontmatter.date}</p>
+          </li>
+        ))}
+        ;
+      </ol>
     </Layout>
   );
 };
