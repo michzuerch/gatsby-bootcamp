c8aad94a3e88feeec7051b6c51db43ef39e485c6
Michael Zuercher <michzuerch@gmail.com>
2020-07-20 21:54:30 +0200

    contentful second version with content

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import Layout from '../components/layout';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
-// export const query = graphql`
-//   query($slug: String!) {
-//     markdownRemark(fields: { slug: { eq: $slug } }) {
-//       frontmatter {
-//         title
-//         date
-//       }
-//       html
-//     }
-//   }
-// `;
+import Layout from '../components/layout';
 
 export const query = graphql`
   query($slug: String!) {
     contentfulBlogPost(slug: { eq: $slug }) {
       title
       publishedDate(formatString: "MMMM Do, YYYY")
+      body {
+        json
+      }
     }
   }
 `;
@@ -27,6 +20,8 @@ const Blog = (props) => (
   <Layout>
     <h1>{props.data.contentfulBlogPost.title}</h1>
     <p>{props.data.contentfulBlogPost.publishedDate}</p>
+    {documentToReactComponents(props.data.contentfulBlogPost.body.json)}
   </Layout>
 );
+
 export default Blog;
